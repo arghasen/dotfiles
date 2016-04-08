f92d4113fc4974603e668b7a3187732b25ea46ad
Argha Sen <iamslowdeath@gmail.com>
2016-04-08 15:42:06 +0530

    update install location

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This is a simple script, but I am too lazy to change it as I dont need more flexibility
-ln -s ./vimrc ~/.vimrc
-ln -s ./zshrc ~/.zshrc
-ln -s ./prompt.sh ~/.prompt.sh
-ln -s ./tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/prompt.sh ~/.prompt.sh
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
